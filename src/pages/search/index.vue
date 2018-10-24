fc31c47b6edc25bf6ffcd0eb423586c9715adca4
Yiqiang Zhong <yiqiang.zhong@yitu-inc.com>
2018-10-24 19:11:07 +0800

    first commit: clone from the quick-start project

---
+++ src/pages/search/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <p>{{motto}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+  },
+
+  methods: {
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style scoped>
+</style>
