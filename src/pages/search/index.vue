7b6137189a3e811ff6bd0d2870199ba11d540ad7
Yiqiang Zhong <yiqiang.zhong@yitu-inc.com>
2018-10-24 21:23:30 +0800

    add simple inputs and images

--- src/pages/search/index.vue
+++ src/pages/search/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="container">
-    <p>{{motto}}</p>
+    <div id="airports">
+      <input id="dep-airport" v-model="dep" />
+      <img id="exchange" src="../../../static/exchange.png" />
+      <input id="arr-airport" v-model="arr" />
+    </div>
+
+    <div id="filters">
+      <!--checkbox for unions-->
+      <!--picker for airlines-->
+      <!--switch for switchers-->
+    </div>
+
+    <div id="buttons">
+      <div class="space"></div>
+      <img id="reset" src="../../../static/reset.png" />
+      <div class="space"></div>
+      <img id="search" src="../../../static/search.png" />
+      <div class="space"></div>
+    </div>
   </div>
 </template>
 
@@ -8,8 +26,16 @@
 export default {
   data () {
     return {
-      motto: 'Hello World',
-      userInfo: {}
+      union: '',
+      ftype: '',
+      dep: 'PEK',
+      depType: '',
+      aircode: '',
+      arr: 'SZX',
+      arrType: '',
+      isStop: '',
+      isCross: '',
+      isConnect: ''
     }
   },
 
@@ -25,4 +51,45 @@ export default {
 </script>
 
 <style scoped>
+  #airports {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: solid 1px #aeaeae;
+    border-bottom: solid 1px #aeaeae;
+    height: 20vh;
+  }
+  #dep-airport {
+    text-align: left;
+    padding-left: 10vw;
+    flex: 1;
+  }
+  #arr-airport {
+    text-align: right;
+    padding-right: 10vw;
+    flex: 1;
+  }
+  #exchange {
+    width: 10vw;
+    height: 10vw;
+  }
+
+  #buttons {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: flex-end;
+    height: 15vh;
+    width: 100%;
+  }
+  #reset {
+    width: 11vw;
+    height: 11vw;
+  }
+  #search {
+    width: 12vw;
+    height: 12vw;
+  }
+  #buttons .space {
+    width: 12vw;
+  }
 </style>
